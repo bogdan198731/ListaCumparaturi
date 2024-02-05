559816f534e32f0807cd60837bca36903aec84ff
bogdan198731 <bogdan198731@gmail.com>
2024-02-05 18:21:10 +0200

    Modificari format

--- src/app/hamburger-menu/hamburger-menu.component.ts
+++ src/app/hamburger-menu/hamburger-menu.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-hamburger-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, RouterLink],
+  imports: [MatButtonModule, MatMenuModule, RouterLink, MatIconModule],
   templateUrl: './hamburger-menu.component.html',
-  styleUrl: './hamburger-menu.component.css'
+  styleUrl: './hamburger-menu.component.scss'
 })
 export class HamburgerMenuComponent {
   isOpen: boolean = false;
