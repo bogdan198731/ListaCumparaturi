5d43ade81423503715a43551e948a8101035590b
bogdan198731 <bogdan198731@gmail.com>
2024-02-19 16:02:53 +0200

    Modificare pagina start pt LP

--- src/app/bun-venit/bun-venit.component.ts
+++ src/app/bun-venit/bun-venit.component.ts
@@ -3,14 +3,16 @@ import { ListaCumparaturiComponent } from "../lista-cumparaturi/lista-cumparatur
 import { VizualizareListaParticularaComponent } from "../vizualizare-lista-particulara/vizualizare-lista-particulara.component";
 import { CautareListeParticulareComponent } from "../cautare-liste-particulare/cautare-liste-particulare.component";
 import { Router, RouterLink } from '@angular/router';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
     selector: 'app-bun-venit',
     standalone: true,
     templateUrl: './bun-venit.component.html',
-    styleUrl: './bun-venit.component.css',
-    imports: [ListaCumparaturiComponent, VizualizareListaParticularaComponent, CautareListeParticulareComponent, RouterLink]
+    styleUrl: './bun-venit.component.scss',
+    imports: [ListaCumparaturiComponent, VizualizareListaParticularaComponent, CautareListeParticulareComponent, RouterLink, MatExpansionModule]
 })
 export class BunVenitComponent {
-
+    panelOpenState = false;
+    test = true;
 }
--- src/app/cautare-liste-particulare/cautare-liste-particulare.component.ts
+++ src/app/cautare-liste-particulare/cautare-liste-particulare.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ListeParticulare } from '../servicii/listeParticulare';
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cautare-liste-particulare',
   standalone: true,
-  imports: [NgIf, NgFor, RouterLink],
+  imports: [NgIf, NgFor, RouterLink, NgClass],
   templateUrl: './cautare-liste-particulare.component.html',
   styleUrl: './cautare-liste-particulare.component.scss'
 })
@@ -14,9 +14,9 @@ export class CautareListeParticulareComponent implements OnInit{
 
   numeListeParticulare:string[] = [];
 
+  @Input() test = false;
   constructor(private listeParticulare:ListeParticulare){}
   ngOnInit(): void {
-
     console.log("nume liste particulare : " ,this.listeParticulare.recuperareNumeListeParticulare());
     this.numeListeParticulare = this.listeParticulare.recuperareNumeListeParticulare();
   }
